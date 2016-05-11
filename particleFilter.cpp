1d3fbccd6d8cf9ecea7debfe10f14593dc5b5ffa
gxyEPFL <xinyi.guo@epfl.ch>
2016-05-11 17:21:58 +0200

    try to maintain a particle array, remove if the weight is high

--- particleFilter.cpp
+++ particleFilter.cpp
@@ -13,7 +13,7 @@
 #include <sstream>
 #include <iostream>
 #include <opencv2/ml/ml.hpp>
-
+#include <math.h>
 static string descriptorVectorFile = "/Users/xinyiguo/SonyCPPGithub/tracker/genfiles/descriptorvector.dat";
 using namespace std;
 using namespace cv;
@@ -21,8 +21,9 @@ double sigmaX = 1;
 double sigmaY = 1;
 int width = 24;
 int height = 48;
+static int removefLAG = 100;
 particleFilter::particleFilter() {
-    totalParticles = 200;
+    totalParticles = 180;
     gsl_rng_env_setup();
     rng = gsl_rng_alloc(gsl_rng_mt19937);
     gsl_rng_set(rng, time(NULL));
@@ -40,12 +41,11 @@ particleFilter::~particleFilter() {
 
 /* for each detection in initial frame, add Nparcitles with weight 0 in the place with origial weight */
 void particleFilter::initParticles(vector<Rect> region,  gsl_rng* rng) {
-    int width = region.at(0).width;
-    int height = region.at(0).height;
+    region.erase (region.begin(),region.begin()+2);
     int orgTarget = region.size();
     cout<<"original target" << orgTarget << endl;
-    int eachFilter = totalParticles/10;
-    nFilters = orgTarget;
+    nFilters = 9;
+    int eachFilter = totalParticles/nFilters;
     for (int i=0; i < totalParticles; i++) {
         int j= i/eachFilter;
         particles[i].xp = particles[i].x = region.at(j % orgTarget).x + gsl_ran_gaussian(rng, sigmaX*sigmaX); //left top of the target
@@ -67,10 +67,8 @@ void particleFilter::initParticles(vector<Rect> region,  gsl_rng* rng) {
      for(int i=0; i<totalParticles; i++){
         particle pi = particles[i];
         particle pn;
-        //double x =  pi.xp + pi.velocityXp + 5 * gsl_ran_gaussian(rng, sigmaX);
-        //double y =  pi.yp + pi.velocityYp + 5 * gsl_ran_gaussian(rng, sigmaX);
-        double x =  pi.xp + 3 * gsl_ran_gaussian(rng, 1);
-        double y =  pi.yp + 3 * gsl_ran_gaussian(rng, 1);
+        double x =  pi.x +  gsl_ran_gaussian(rng, 1);
+        double y =  pi.y +  gsl_ran_gaussian(rng, 1);
         pn.x = (float) MAX( 0.0, MIN((float) w - width, x));
         pn.y = (float) MAX( 0.0, MIN((float) h - height, y));
         pn.xp = pi.x;
@@ -87,32 +85,40 @@ void particleFilter::initParticles(vector<Rect> region,  gsl_rng* rng) {
  }
 
  /*update transated particle weight based on similarities with the previous detected hog discriptor*/
- void particleFilter::updateWeight(Mat& frame,  const HOGDescriptor& hog){
+ void particleFilter::updateWeight(Mat& frame,  const HOGDescriptor& hog ){
      for(int i = 0; i < totalParticles; i++){
-        particles[i].wp = particles[i].w;
-        particles[i].w = computerSim(hog, frame, particles[i].x, particles[i].y, 0);
-        cout << particles[i].w << endl;
+         if(particles[i].id!=removefLAG){
+            double temp = particles[i].wp;
+            particles[i].wp = particles[i].w;
+            particles[i].w = computerSim(hog, frame, particles[i].x, particles[i].y, 0)
+                    *dynamic_prob(&particles[i])/proposalP(hog, frame, &particles[i], 0.5, 0);
+            particles[i].w =computerSim(hog, frame, particles[i].x, particles[i].y, 0);
+         }
     }
     cout << "finish calculation" << endl;
-      //normalized particle weight
+
+    //normalized particle weight
     double eachFilterSum[nFilters];
     for(int i=0; i < totalParticles; i++){
         int id = particles[i].id;
-        eachFilterSum[id] += particles[i].w;
+        if(id != removefLAG)
+            eachFilterSum[id] += particles[i].w;
     }
 
      for(int i=0; i < totalParticles; i++){
-        if(eachFilterSum[particles[i].id]!=0){
+        if(eachFilterSum[particles[i].id]!=0 && particles[i].id!= removefLAG){
             particles[i].wNormalized = particles[i].w / eachFilterSum[particles[i].id];
         }
         else particles[i].wNormalized = 0;
     }
     cout << "normalize finish" << endl;
+
     //update each filter's weight
     double eachFilterW[nFilters];
     for(int i=0; i< totalParticles; i++){
         int id = particles[i].id;
-        eachFilterW[id] += particles[i].w;
+        if(id != removefLAG)
+            eachFilterW[id] += particles[i].w;
     }
     double sigmaweight =0; // simga pi n t-1 * wnt
     for(int j=0; j<nFilters; j++){
@@ -124,27 +130,8 @@ void particleFilter::initParticles(vector<Rect> region,  gsl_rng* rng) {
         cout << "filter " << j << "weight is" << filters[j].weight << endl;
         filters[j].weightPrev = temp;
     }
-    double checkSum = 0;
-    for(int j=0; j<nFilters; j++){
-        checkSum += filters[j].weight;
-    }
-    cout << "final weight is " << checkSum << endl;
  }
 
-//http://docs.opencv.org/master/d5/d33/structcv_1_1HOGDescriptor.html#gsc.tab=0
-//compute sim based on the exsiting hog descriptor and detector
-double particleFilter::computerSim(const HOGDescriptor& hog, Mat& frame, int x, int y, double hitThreshold){
-        vector<Point> searchLoc;
-        vector<Point> foundLoc;
-        vector<double> weights;
-        searchLoc.push_back(Point((double)x, (double)y));
-        //cout << "search location is data point "<< x << " " << y << endl;
-        hog.detect(frame, foundLoc, weights, hitThreshold, Size(24,48), Size(1,1), searchLoc);
-        if(weights.empty()) return 0;
-        //cout << "find location is point"<< foundLoc.at(0).x << " " << foundLoc.at(0).y << endl;
-        //cout << "compute sim result is " << weights.at(0) << endl;
-        return MAX(0,weights.at(0)-2.5);
-}
 
 void particleFilter::resample(gsl_rng* rng){
     particle * newParticles;
@@ -153,45 +140,131 @@ void particleFilter::resample(gsl_rng* rng){
     double beta = 0.0;
     double maxWeight =0;
     for (int i=0; i < totalParticles; i++) {
-        maxWeight = max(maxWeight, particles[i].wNormalized* filters[particles[i].id].weight);
+        if(particles[i].id != removefLAG)
+            maxWeight = max(maxWeight, particles[i].wNormalized* filters[particles[i].id].weight);
     }
     for(int j=0; j < totalParticles; j++){
+        if(particles[j].id == removefLAG){
+            newParticles[j] = particles[j];
+            continue;
+        }
         beta += (rng, 0, 2*maxWeight);
         while(beta > particles[index].wNormalized* (filters[particles[index].id].weight)){
             beta -= particles[index].wNormalized * (filters[particles[index].id].weight);
             index = (index+1) % totalParticles;
         }
         while(particles[index].w==0){
-                 index = (index+1) % totalParticles;
+                 index = (index+1) % (totalParticles);
         }
-       newParticles[j] = particles[index];
+        newParticles[j] = particles[index];
     }
+    cout << "Resample finished partly" << endl;
     for(int i=0; i<totalParticles; i++){
         particles[i] = newParticles[i];
-        particles[i].w = particles[i].wNormalized = 1/totalParticles;
+        //particles[i].w = particles[i].wNormalized = 1/totalParticles;
+    }
+    free(newParticles);
+    vector<int> countP1;
+    for(int i=0; i<nFilters; i++){
+        countP1.push_back(0);
+    }
+    for(int i=0; i< totalParticles; i++){
+        if(particles[i].id!=removefLAG)
+            countP1.at(particles[i].id)++;
+    }
+    for(int i=0; i< nFilters; i++){
+        filters[i].nParticles = countP1.at(i);
+    }
+    for(int i=0; i< nFilters; i++){
+        if(filters[i].nParticles > 20){
+            cout << "call the remove function" << endl;
+            removeParticles(i);
+        }
     }
     vector<int> countP;
     for(int i=0; i<nFilters; i++){
         countP.push_back(0);
     }
     for(int i=0; i< totalParticles; i++){
-        countP.at(particles[i].id)++;
+        if(particles[i].id!=removefLAG)
+            countP.at(particles[i].id)++;
     }
     for(int i=0; i< nFilters; i++){
         filters[i].nParticles = countP.at(i);
     }
     for(int i=0; i< nFilters;i++)
         cout << "n particle value is " << filters[i].nParticles << endl;
-    free(newParticles);
+
+    int Remove = 0 ;
+    for(int i=0; i< totalParticles; i++){
+        if(particles[i].id==removefLAG)
+            Remove ++ ;
+    }
+    for(int i=0; i< totalParticles; i++){
+        if(particles[i].id==removefLAG){
+            particles[i].w = particles[i].wNormalized = 1.0/(totalParticles - Remove);
+        }
+    }
 }
 
+void particleFilter::removeParticles(int i){
+    map <double, int> m;
+    for(int j=0; j<totalParticles; j++){
+        if(particles[j].id == i){
+            m[particles[j].w] = j;
+        }
+    }
+    //sort the map by key
+    int count = 0;
+    typedef map<double, int>::iterator it_type;
+    for(it_type iterator = m.begin(); iterator != m.end(); iterator++) {
+        count ++;
+        if(count <= filters[i].nParticles - 20){
+           int id = iterator-> second;
+           //cout << "check order or not " <<  iterator-> first << endl;
+           particles[id].id = removefLAG;
+           particles[id].x = particles[id].y = particles[id].w = particles[id].wNormalized = 0;
+       }
+    }
+    filters[i].nParticles = 20;
+}
+
+
+//http://docs.opencv.org/master/d5/d33/structcv_1_1HOGDescriptor.html#gsc.tab=0
+//compute sim based on the exsiting hog descriptor and detector apperance model
+double particleFilter::computerSim(const HOGDescriptor& hog, Mat& frame, int x, int y, double hitThreshold){
+        vector<Point> searchLoc;
+        vector<Point> foundLoc;
+        vector<double> weights;
+        searchLoc.push_back(Point((double)x, (double)y));
+        hog.detect(frame, foundLoc, weights, hitThreshold, Size(24,48), Size(1,1), searchLoc);
+        if(weights.empty())
+            return 0;
+        return MAX(0,weights.at(0)-2.8);
+}
+
+//dynamic model
+double particleFilter::dynamic_prob(particle *p){
+    double xProbability = gsl_ran_gaussian_pdf (p->x - p->xp, sigmaX);
+    double yProbability = gsl_ran_gaussian_pdf (p->y - p->yp, sigmaY);
+//return sqrt(xProbability*yProbability);
+  return xProbability;
+}
+
+//proposal distribution
+double particleFilter::proposalP(const HOGDescriptor& hog, Mat& frame, particle *p, double alpha, double hitThreshold){
+    double apperance =  computerSim(hog, frame, p -> x , p -> y, hitThreshold);
+    double dynamic = dynamic_prob(p);
+return alpha * apperance + (1-alpha)*dynamic;
+}
 void particleFilter::displayParticles(Mat& frame){
     for(int i=0; i<totalParticles; i++){
-        int x0 = cvRound(particles[i].x);
-        int y0 = cvRound(particles[i].y);
-        Rect r = Rect(min(720-width, x0), min(480-height,y0), width, height);
-        //cout << "particle belongs to group's size" << filters[particles[i].id].nParticles << endl;
-        rectangle(frame, r, Scalar(64, 255, 64), 1);
+        //if(particles[i].id != removefLAG){
+            int x0 = cvRound(particles[i].x);
+            int y0 = cvRound(particles[i].y);
+            Rect r = Rect(min(720-width, x0), min(480-height,y0), width, height);
+            rectangle(frame, r, Scalar(64, 255, 64), 1);
+        //}
     }
     imshow("result",frame);
     waitKey(4000);
